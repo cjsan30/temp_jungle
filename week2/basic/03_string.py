81f75632be1716970078f4c3a5ebdfdab8336bed
minseok <cjsan30@gmail.com>
2026-03-09 17:01:58 +0900

    solve 03_string.py

--- week2/basic/03_string.py
+++ week2/basic/03_string.py
@@ -36,14 +36,31 @@ def is_palindrome(s):
     """
     # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
     # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
+
+    if s.isalnum() == True:
+        s = s.lower()
+    else:
+        rst = (s.strip()).lower()
+        s = ''.join(i for i in rst if i.isalnum())
+
     
     # TODO: 정제된 문자열이 회문인지 확인하세요
     # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
     # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
-    pass
     
-    #return False
+    rst = ''.join(reversed(s))
+    if s == rst:
+        return True
+    else:
+        return False
+    
+    
+    
+    # for i in range(len(s)):
+    #     if s[i] != s[i-1]:
+    #         return False
+    #     else:
+    #         return True
 
 # 테스트 케이스
 if __name__ == "__main__":
