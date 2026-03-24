88e00b7ec00c1e31a0b09d1dbbd6a9d1e6d6ef2a
minseok <cjsan30@gmail.com>
2026-03-24 10:43:42 +0900

    solve low_graph/DFS/BFS

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+
+from collections import deque
+
+computer = int(input())
+line = int(input())
+# con = [ tuple(map(int, input().split())) for _ in range(line)]
+
+con = [[] for _ in range(computer+1)]
+
+for _ in range(line):
+    a, b = map(int, input().split())
+    con[a].append(b)
+    con[b].append(a)
+
+# zombie : 감염 체크 현황(재탐색 방지)
+# qu : 감염 처리된 목록
+zombie = [False for _ in range(computer+1)]
+qu = deque([1])
+zombie[1] = True
+
+while qu:
+    zompc = qu.popleft() # 1
+    
+    for x in con[zompc]:
+        if not zombie[x]:
+            zombie[x] = True
+            qu.append(x)
+
+print(zombie.count(True)-1)
--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,32 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+
+from collections import deque
+
+areaN = int(input())
+area = []
+for _ in range(areaN):
+    area.append(list(map(int, input().split())))
+## BFS
+visited = [[False] * areaN for _ in range(areaN)]
+qu = deque()
+qu.append((0,0))
+visited[0][0] = True
+
+while qu:
+    y,x = qu.popleft()
+    if y == areaN-1 and x == areaN-1:
+        print("HaruHaru")
+        break
+
+    jump = area[y][x]
+    if jump == 0: continue
+    
+    for jmy, jmx in [(jump,0), (0,jump)]:
+        ry, rx = y+jmy, x+jmx
+        if ry < areaN and rx < areaN and not visited[ry][rx]:
+            visited[ry][rx] = True
+            qu.append((ry,rx))
+    
+else:
+    print("Hing")
