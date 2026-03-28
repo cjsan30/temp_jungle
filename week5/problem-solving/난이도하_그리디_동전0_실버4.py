5de77e2495b4519731b815441b25fd0587d05212
msPark <cjsan30@gmail.com>
2026-03-28 16:09:57 +0900

    solve low_greedy_coin

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,21 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+
+# cointype[::-1]
+# total / cointype >> append total
+# total % cointype[i]
+
+(cointype, total) = tuple(map(int,input().split()))
+
+result = 0
+ct = []
+for _ in range(cointype):
+    ct.append(int(input()))
+ct = ct[::-1]
+
+for i in range(len(ct)):
+    result = result + int(total / ct[i])
+    total = total % ct[i]
+   
+print(result)
