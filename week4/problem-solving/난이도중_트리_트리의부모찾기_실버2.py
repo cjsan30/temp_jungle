9d6049d04b827ae7c469e167db8901a9baf46ee9
msPark <cjsan30@gmail.com>
2026-03-28 14:02:13 +0900

    solve beakjoon

--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,31 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+
+nodenum =  int(input())
+
+con = [[] for _ in range(nodenum+1)]
+
+for _ in range(nodenum-1):
+    a, b = map(int, input().split())
+    con[a].append(b)
+    con[b].append(a)
+    
+visited = [False] * (nodenum+1)
+prnt = [[] for _ in range(nodenum+1)]
+# prnt = [[0] * (nodenum+1)]
+
+def dfs(now, visited):
+    visited[now] = True
+    for next in con[now]:
+        if not visited[next]:
+            prnt[next] = now
+            dfs(next, visited)
+    return prnt
+            
+dfs(1,visited)
+
+for x in range(2, len(prnt)):
+    print(prnt[x])
--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,37 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+# 사소한 디테일 신경 안써서 수정 너무 많았음. 
+
+
+# 0, 1 쓰여진 타일 선물 ( 아빠가 타일 공장장 )
+# 1 , 00 타일   >> 01, 10 조합 불가 | 00 , 11
+
+# 2진 수열의 개수 : t[n]    : 피보나치, Bottom-Up으로 접근
+# 1     1   1
+# 2     2   00,11
+# 3     3   001,100,111
+# 4     5   0000,0011,1001,1100,1111
+
+# a = 1
+# b = 2
+
+# for _ in range(2, tile):
+#     a, b = b, a+b
+    
+# print(b % 15746)
+
+tile = int(input())
+def dadtile(n):
+    if n == 1: return 1
+
+    t = [0] * (n+1)
+    t[1] = 1
+    t[2] = 2    
+
+    for i in range(3, n+1):
+        t[i] = (t[i-1] + t[i-2]) % 15746
+
+    return t[n]
+
+print(dadtile(tile))
